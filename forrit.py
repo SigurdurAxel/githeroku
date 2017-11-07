4064de2be4f7895e3695cf67cfe72aa8f3f3b391
SigurdurAxel <posno123@gmail.com>
2017-11-07 12:17:09 +0000

    bla bla

--- forrit.py
+++ forrit.py
@@ -3,6 +3,6 @@ from bottle import *
 
 @route('/')
 def index():
-    return "Bottle forrit í pyCharm"
+    return "Bottle forrit í pyCharm bla bla "
 
 run(host='0.0.0.0', port=os.environ.get('PORT'))
